abc1f37ea14cca2eaf234fb5d2f00ca300959456
Walter Molano <wagimo@gmail.com>
2020-06-23 20:38:56 -0500

    modificaciones en modulos y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graph1Component } from './pages/graph1/graph1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { RegistersComponent } from './login/registers.component'
 
-
+//modulos
+import { PagesModule } from  './pages/pages.module'
 //rutas
 import { APP_ROUTES  } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { RegistersComponent } from './login/registers.component'
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graph1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegistersComponent,
-    
+    LoginComponent,    
+    RegistersComponent    
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    APP_ROUTES
+    BrowserModule,    
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
